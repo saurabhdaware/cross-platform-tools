04113d77cda793dc3149266c5426a0adec9132f5
saurabhdaware <saurabhdaware99@gmail.com>
2023-11-21 00:23:16 +0530

    feat: different type files

--- src/addition.native.ts
+++ src/addition.native.ts
@@ -1,3 +1,8 @@
-export const addition = (a: number, b: number): number => {
-  return a + b;
+type NumLike = string | number;
+export const addition = (a: NumLike, b: NumLike): number => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+  
+  return 0;
 }
